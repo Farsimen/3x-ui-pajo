274a62f00ebe1b4b1605866a75aa9238447b9930
Pajo <79488643+Farsimen@users.noreply.github.com>
2025-12-09 03:20:56 +0330

    Fix import paths in index controller

--- web/controller/index.go
+++ web/controller/index.go
@@ -5,9 +5,9 @@ import (
 	"text/template"
 	"time"
 
-	"x-ui/logger"
-	"x-ui/web/service"
-	"x-ui/web/session"
+	"github.com/mhsanaei/3x-ui/v2/logger"
+	"github.com/mhsanaei/3x-ui/v2/web/service"
+	"github.com/mhsanaei/3x-ui/v2/web/session"
 
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
